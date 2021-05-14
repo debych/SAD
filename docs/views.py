69f6d9822f6965f2d9f6d89d871f05f157af0170
yegor <yegorpereverzev@gmail.com>
2021-05-14 17:23:23 +0300

    add register form

--- docs/views.py
+++ docs/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth.models import Group
 
 # Create your views here.
 def index(request):
-    return HttpResponse('kek')
+    context = {}
+    return render(request, 'docs/index.html', context)
+
+def register(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+        user = User.objects.get(username=request.POST.get('username'))
+        default_group = Group.objects.get(name='default')
+        user.groups.add(default_group)
+        user.is_staff = True
+        user.save()
+        return redirect('/admin')
+
+    context = {'form': form}
+    return render(request, 'docs/register.html', context)
